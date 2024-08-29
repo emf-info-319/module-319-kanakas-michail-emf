d82e7cafeaad7ad04870823d314fb4ca225a8d24
Michail Kanakas <michail.kanakas@studentfr.ch>
2024-08-29 11:43:32 +0200

    j

--- exercices/exercice02/Exercice02.java
+++ exercices/exercice02/Exercice02.java
@@ -1,5 +1,24 @@
 package exercices.exercice02;
 
-public class Exercice02 {
+public class Exercice02 {public static void main(String[] args) {
+    System.out.println("Bonjour tout le monde!");
+    System.out.println("Ca va ?");
+    boolean x = false;
+    System.out.println(x);
+    boolean y = true;
+    System.out.println(y);
+    char exemple = 'B';
+    System.out.println(exemple);
+    byte tresPetitNombre = 100;
+    System.out.println(tresPetitNombre);
+    short petitNombre = 1200;
+    int nombre = 1249;
+    long grandNombre = 35885385;
+    System.out.println(petitNombre);
+    System.out.println(nombre);
+    System.out.println(grandNombre);
+    double flottantDouble = 314896346341.5;
+    System.out.println(flottantDouble);
+} ;
     
-}
+} ;
---
+++ exercices/exercice03/Exercice03.java
@@ -0,0 +1,12 @@
+package exercices.exercice03;
+
+public class Exercice03 {
+    public static void main(String[] args) {
+        int age = 15;
+        String nom = "Michail";
+        int ageFutur = age + 10;
+        System.out.println("Je m’appelle " + nom + " et j’ai "+ age + " années.");
+        System.out.println("Dans 10 ans, j'aurai " + ageFutur + " ans");
+    }
+    
+}

51eb4a7588f0790b905d4b40b3ef4fb374e0684a
Michail Kanakas <michail.kanakas@studentfr.ch>
2024-09-13 07:29:44 +0200

    etrh

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -3,6 +3,7 @@ package devoirs.devoir02;
 public class devoir02 {
     public final static int CAPACITE_RESERVOIR_A = 3;
     public final static int CAPACITE_RESERVOIR_B = 5;
+
     public static void main(String[] args) {
         int reservoirA=0;
         int reservoirB=0;
@@ -13,15 +14,17 @@ public class devoir02 {
         System.out.println("Il y a "+remplissage+" litres à remplir.");
 
         while (remplissage!=0) {
-            if (reservoirA<=3) {
-                
-            }
+            if (reservoirA<3) {
+            reservoirA++;
+            System.out.println("Remplissage du réservoir A...");
+        } else {
+            reservoirB++;
+            System.out.println("Remplissage du réservoir B...");
         }
-
-
-
-
-
+    remplissage--;
+    System.out.println("Le réservoir A : "+reservoirA+", le réservoir B : "+reservoirB);
+    }
         
+        System.out.println("Remplissage terminé!");
     }
 }

2d4dbe69a9897a17ec42b4b13e78b5493c8bdb07
Michail Kanakas <michail.kanakas@studentfr.ch>
2024-10-31 08:10:24 +0100

    hhh

--- exercices/IntroMethode.java
+++ exercices/IntroMethode.java
@@ -2,6 +2,6 @@ package exercices;
 
 public class IntroMethode {
     public static void main(String[] args) {
-        
+        System.out.println("Bonjour et bienvenue sur ce premier programme.\nOn va voir les méthodes dans ce cours.");
     }
 }

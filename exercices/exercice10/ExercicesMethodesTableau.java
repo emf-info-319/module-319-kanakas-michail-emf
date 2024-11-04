332989501c49d5baa25d18763a902ac1daa1662e
Michail Kanakas <michail.kanakas@studentfr.ch>
2024-11-04 22:31:47 +0100

    b

--- exercices/exercice10/ExercicesMethodesTableau.java
+++ exercices/exercice10/ExercicesMethodesTableau.java
@@ -5,6 +5,7 @@ public class ExercicesMethodesTableau {
         int size = 3;
         System.out.println("Le tableau a une taille de "+size+" cellules\n");
         int[] nomDuTab = tabSelonTaille(size);
+        //1
 
         int valeurARemplir = 56;
         int[] tab = new int[6];
@@ -28,6 +29,33 @@ public class ExercicesMethodesTableau {
 
         int[] tableauValeurMax = {12,23, 8, 78};
         System.out.println(maxTab(tableauValeurMax));
+
+        int[] tableauContage = {12,23, 8, 78};
+        int valeurAConter = 8;
+        System.out.println(contageValeur(tableauContage, valeurAConter));
+
+        int[] tableauSomme = {12,23, 8, 78};
+        System.out.println(sommeTab(tableauSomme));
+
+        double[] tableauMoyenne = {12,23, 8, 78};
+        System.out.println(moyenneTab(tableauMoyenne));
+
+        int[] tableauRemplacement = {12,23, 8, 78};
+        int recherche = 9;
+        int remplacement = 56;
+        int []tableauRemplacementEffectuer = remplacementTab(tableauRemplacement, recherche, remplacement);
+        for (int i = 0; i < tableauRemplacement.length; i++) {
+            System.out.println(tableauRemplacementEffectuer[i]);
+        }
+        
+        int[] tableauPremiereOccurence = {12,23, 8, 78, 23};
+        int recherchePremiereOccurence = 23;
+        System.out.println(premierOccurenceTab(tableauPremiereOccurence, recherchePremiereOccurence));
+
+        int[] tableauDerniereOccurence = {12,23, 8, 78, 23, 45, 65, 23};
+        int rechercheDerniereOccurence = 23;
+        System.out.println(derniereOccurenceTab(tableauDerniereOccurence, rechercheDerniereOccurence));
+
     }
 
     public static int[] tabSelonTaille(int taille){
@@ -75,5 +103,70 @@ public class ExercicesMethodesTableau {
         return maxTemp;
     }
 
+    public static int contageValeur(int[] tab, int valeur) {
+        int nombreDeFois = 0;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i]==valeur) {
+                nombreDeFois++;
+            }
+        }
+        return nombreDeFois;
+    }
 
+    public static int sommeTab(int[] tab) {
+        int tabTotal = 0;
+        for (int i = 0; i < tab.length; i++) {
+        tabTotal+=tab[i];
+        }
+        return tabTotal;
+    }
+
+    public static double moyenneTab(double[] tab) {
+        double tabMoyenne= 0;
+        for (int i = 0; i < tab.length; i++) {
+        tabMoyenne+=tab[i];
+        }
+        tabMoyenne=tabMoyenne/tab.length;
+        return tabMoyenne;
+    }
+
+    public static int[] remplacementTab(int[] tab, int recherche, int remplacement) {
+        for (int i = 0; i < tab.length; i++) {
+        if (tab[i]==recherche) {
+            tab[i]=remplacement;
+        }
+        }
+        return tab;
+    }
+
+    public static int premierOccurenceTab(int[] tab, int recherche) {
+        int occurence = -1;
+        for (int i = 0; i < tab.length; i++) {
+           if (tab[i]==recherche) {
+            occurence=i;
+            break;
+           } 
+        }
+        return occurence;
+    }
+
+    public static int derniereOccurenceTab(int[] tab, int recherche) {
+        int[] occurences = new int[tab.length];
+        int nbOccurence = 0;
+        for (int i = 0; i < tab.length; i++) {
+           if (tab[i]==recherche) {
+            occurences[nbOccurence] = i;
+            nbOccurence++;
+           } 
+        }
+        int derniereOccurence = -1;
+        int derniereOccurenceTemp = 0;
+        if (nbOccurence!=0) {
+            for (int i = 0; i < occurences.length; i++) {
+                derniereOccurence = Math.max(occurences[i], derniereOccurenceTemp);
+                derniereOccurenceTemp = derniereOccurence;
+            }
+        }
+        return derniereOccurence;
+    }
 }

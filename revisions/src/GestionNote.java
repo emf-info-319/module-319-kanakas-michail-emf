0e290d1a5ca9f5a09344850964ea3d85785708df
Michail Kanakas <michail.kanakas@studentfr.ch>
2024-11-21 11:46:39 +0100

    bfgr

---
+++ revisions/src/GestionNote.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class GestionNote {
+    public static void main(String[] args) {
+        System.out.print("Quelle opération voulez-vous exécuter : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+    }
+}
